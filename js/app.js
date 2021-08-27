7d695fb9b13a653f493d6ce99659e811f27c2764
Kailey Waal <15kwaal@gmail.com>
2021-08-27 13:02:16 -0600

    debugged light mode toggle, added project details component

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+window.onload = adjustLightModeSlider;
+window.onload = getThemeFromLocalStorage;
+
 function toggleMenu(e) {
     if(e.type != "resize") {
         document.querySelector('.hamburger-icon').classList.toggle('collapsible--open');
@@ -13,7 +16,11 @@ function toggleMenu(e) {
 window.addEventListener("resize", toggleMenu);
 
 // Get dark mode from local storage
-localStorage.getItem('theme') === 'dark-mode' ? setTheme('dark-mode') : setTheme('light-mode');
+
+function getThemeFromLocalStorage() {
+    localStorage.getItem('theme') === 'dark-mode' ? setTheme('dark-mode') : setTheme('light-mode');
+    adjustLightModeSlider();
+}
 
 // Set theme by passing the corresponding class name as an argument
 function setTheme(themeName) {
@@ -26,10 +33,23 @@ function setTheme(themeName) {
 // Add event listener to toggle button and call this function to toggle the theme.
 function toggleTheme() {
     const currentTheme = localStorage.getItem('theme') === 'dark-mode';
+    // if currentTheme is dark mode, change the checkbox to checked so the toggle is in correct position
+    
     // If current theme is dark mode, change to light mode
     //If current theme is light mode, change to dark mode
     //If no current theme is in storage, set to light mode
     currentTheme ? setTheme('light-mode') : setTheme('dark-mode');
-
     //OPTIONAL: adjust style settings of toggle button here
+    adjustLightModeSlider();
 }
+
+function adjustLightModeSlider() {
+    let checkbox = document.querySelector('#checkbox');
+    if(localStorage.getItem('theme') === 'dark-mode') {
+        checkbox.checked = true;
+        let slider = document.querySelector(".slider:before");
+        slider.style.transform = "translateX(22px)";
+    }
+}
+
+
